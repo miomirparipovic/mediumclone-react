6aa767d868eb7546e2375b5459e4c8594eb70e33
Miomir Paripovic <deploy.friday@gmail.com>
2023-12-31 20:48:40 +0100

    Add Authentication, Sign in

--- src/PageRoutes.jsx
+++ src/PageRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Article from "./views/article/Article";
 import GlobalFeed from "./views/global-feed/GlobalFeed";
+import Authentication from "./views/authentication/Authentication";
 
 const PageRoutes = () => {
   return (
     <Routes>
       <Route path="/articles/:slug" element={<Article />} />
+      <Route path="/login" element={<Authentication />} />
       <Route path="/" element={<GlobalFeed />} />
     </Routes>
   );
