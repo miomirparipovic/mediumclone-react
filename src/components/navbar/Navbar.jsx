e94e6230d6d04bd09c3d47a2fec44b942d0f265c
Miomir Paripovic <deploy.friday@gmail.com>
2024-01-05 16:27:17 +0100

    Modify navbar for logged in user

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../../contexts/currentUserProvider";
 
 const Navbar = () => {
+  const [currentUserState] = useContext(CurrentUserContext);
+  const userImage =
+    (currentUserState.isLoggedIn && currentUserState.currentUser.image) ||
+    "https://api.realworld.io/images/smiley-cyrus.jpeg";
+
+  console.log("current user state", currentUserState);
+
   return (
     <nav className="navbar navbar-light">
       <div className="container">
@@ -13,16 +22,47 @@ const Navbar = () => {
               Home
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink to="/login" className="nav-link">
-              Sign in
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/register" className="nav-link">
-              Sign up
-            </NavLink>
-          </li>
+          {currentUserState.isLoggedIn && (
+            <>
+              <li className="nav-item">
+                <NavLink to="/articles/new" className="nav-link">
+                  <i className="ion-gear-a" />
+                  &nbsp; New Post
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/settings" className="nav-link">
+                  <i className="ion-compose" />
+                  &nbsp; Settings
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  to={`/profiles/${currentUserState.currentUser.username}`}
+                  className="nav-link"
+                >
+                  <i className="ion-compose" />
+                  <img src={userImage} alt="" className="user-pic" />
+                  &nbsp; {currentUserState.currentUser.username}
+                </NavLink>
+              </li>
+            </>
+          )}
+
+          {currentUserState.isLoggedIn === false && (
+            <>
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Sign in
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">
+                  Sign up
+                </NavLink>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
