75273067111be5251933dff9522e5f777fc89fbe
Miomir Paripovic <deploy.friday@gmail.com>
2024-01-08 13:23:38 +0100

    Add TagFeed, add route

--- src/PageRoutes.jsx
+++ src/PageRoutes.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Article from "./views/article/Article";
 import GlobalFeed from "./views/global-feed/GlobalFeed";
 import Authentication from "./views/authentication/Authentication";
+import TagFeed from "./views/tag-feed/TagFeed";
 
 const PageRoutes = () => {
   return (
     <Routes>
       <Route path="/articles/:slug" element={<Article />} />
+      <Route path="/tags/:slug" element={<TagFeed />} />
       <Route path="/login" element={<Authentication />} />
       <Route path="/register" element={<Authentication />} />
       <Route path="/" element={<GlobalFeed />} />
---
+++ src/views/tag-feed/TagFeed.jsx
@@ -0,0 +1,66 @@
+import { useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import Feed from "../../components/feed/Feed";
+import useFetch from "../../hooks/useFetch";
+import Pagination from "../../components/pagination/Pagination";
+import PopularTags from "../../components/popular-tags/PopularTags";
+import Loading from "../../components/loading/Loading";
+import ErrorMessage from "../../components/error-message/ErrorMessage";
+import { LIMIT, getPaginator, objectToQueryString } from "../../utils";
+import FeedToggler from "../../components/feed-toggler/FeedToggler";
+
+const TagFeed = () => {
+  const location = useLocation();
+  const { slug } = useParams();
+  const { offset, currentPage } = getPaginator(location.search);
+  const stringifiedParams = objectToQueryString({
+    limit: LIMIT,
+    offset,
+    tag: slug,
+  });
+  const apiUrl = `/articles?${stringifiedParams}`;
+  const [{ response, error, isLoading }, doFetch] = useFetch(apiUrl);
+
+  console.log("globalFeed", location);
+  console.log("getpaginator", getPaginator(location.search));
+  console.log("stringified", stringifiedParams);
+  console.log("slug", slug);
+
+  useEffect(() => {
+    doFetch();
+  }, [currentPage, doFetch, slug]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <h1>Medium Clone</h1>
+        <p>A place to share knowledge.</p>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            <FeedToggler tagName={slug} />
+            {isLoading && <Loading />}
+            {error && <ErrorMessage />}
+            {!isLoading && response && (
+              <>
+                <Feed articles={response.articles} />
+                <Pagination
+                  total={response.articlesCount}
+                  limit={LIMIT}
+                  currentPage={currentPage}
+                  url={location.pathname}
+                />
+              </>
+            )}
+          </div>
+          <div className="col-md-3">
+            <PopularTags />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TagFeed;
